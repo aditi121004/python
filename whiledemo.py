64117dee47b37d3a160d9166fc167439d42fe474
aditi121004 <kumbharaditi27@gmail.com>
2025-03-18 09:34:59 +0530

    Add files via upload

---
+++ whiledemo.py
@@ -0,0 +1,20 @@
+# Empty while block
+""" i = 1
+while i == 1: #infinite
+    pass  # but no output """ 
+
+""" while True : # infinite
+    print("Aditi") # output
+    break #stop """
+
+"""while False:
+    print("Aditi")"""
+
+#WAP to print your namefor 10 time
+
+"""
+i = 1
+while i <= 10:
+    print(i, "ADITI")
+    i = i + 1
+    """ 
